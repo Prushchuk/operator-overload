3f0e01a07841ec3bebe8b8ea8707f20352452e64
Prushchuk <maksimbarilak922@gmail.com>
2024-03-16 14:00:34 +0200

    string pop_back

--- ConsoleApplication43.cpp
+++ ConsoleApplication43.cpp
@@ -1,101 +1,55 @@
 #include <iostream>
-using namespace std;
+#include <cstring>
 
-
-class Matrix
-{
+class MyString {
 private:
-    int* data;
-    int cols;
-    int rows;
+    char* data;
+    size_t length;
 
 public:
-    Matrix(int rows, int cols) : rows{ rows }, cols{ cols }
-    {
-        data = new int[rows * cols];
-        for (int i = 0; i < rows * cols; ++i) {
-            data[i] = 0;
+    MyString(const char* str = "") {
+        length = std::strlen(str);
+        data = new char[length + 1];
+        strcpy_s(data, length + 1, str);
+    }
+
+    ~MyString() {
+        delete[] data;
+    }
+
+    void pop_back() {
+        if (length > 0) {
+            data[length - 1] = '\0';
+            --length;
         }
     }
 
-    //Копіювання
-    Matrix(const Matrix& other) : rows{ other.cols }, cols{ other.cols }
-    {
-        data = new int[rows, cols];
-        for (int i = 0; i < rows * cols; i++)
-        {
-            data[i] = other.data[i];
-        }
+    MyString& operator+=(const MyString& other) {
+        char* newData = new char[length + other.length + 1];
+        strcpy_s(newData, length + other.length + 1, data);
+        strcat_s(newData, length + other.length + 1, other.data);
+        delete[] data;
+        data = newData;
+        length += other.length;
+        return *this;
     }
 
-    ~Matrix() {
-        delete[]data;
-    }
-
-
-    Matrix operator+(const Matrix& other) const {
-        Matrix result(rows, cols);
-        for (int i = 0; i < rows * cols; i++) {
-            result.data[i] = data[i] + other.data[i];
-        }
-        return result;
-    }
-
-    Matrix operator-(const Matrix& other) const {
-        Matrix result(rows, cols);
-        for (int i = 0; i < rows * cols; i++)
-        {
-            result.data[i] = data[i] + other.data[i];
-        }
-    }
-
-    Matrix operator*(const Matrix& other) const {
-        Matrix result(rows, other.cols);
-        for (int i = 0; i < rows; i++)
-        {
-            for (int j = 0; j < other.cols; j++)
-            {
-                for (int k = 0; k < cols; k++)
-                {
-                    result.data[i * result.cols + j] += data[i * cols + k] * other.data[k * other.cols + j];
-                }
-            }
-        }
-    }
-
-    friend ostream& operator<<(ostream& os, const Matrix& matrix) {
-        for (int i = 0; i < matrix.rows; ++i) {
-            for (int j = 0; j < matrix.cols; ++j) {
-                os << matrix.data[i * matrix.cols + j] << " ";
-            }
-            os << endl;
-        }
+    friend std::ostream& operator<<(std::ostream& os, const MyString& myStr) {
+        os << myStr.data;
         return os;
     }
 };
 
-int main()
-{
-    Matrix A(2, 2);
-    A(0, 0) = 1;
-    A(0, 1) = 2;
-    A(1, 0) = 3;
-    A(1, 1) = 4;
+int main() {
+    MyString str("Hello");
+    std::cout << "Before: " << str << std::endl;
+    str.pop_back();
+    std::cout << "After: " << str << std::endl;
 
-    Matrix B(2, 2);
-    B(0, 0) = 5;
-    B(0, 1) = 6;
-    B(1, 0) = 7;
-    B(1, 1) = 8;
-
-    Matrix C = A + B;
-    std::cout << "A + B:\n" << C << std::endl;
-
-    Matrix D = A - B;
-    std::cout << "A - B:\n" << D << std::endl;
-
-    Matrix E = A * B;
-    std::cout << "A * B:\n" << E << std::endl;
+    MyString str1("Hello");
+    MyString str2(" world");
+    str1 += str2;
+    std::cout << "After concatenation: " << str1 << std::endl;
 
     return 0;
 }
